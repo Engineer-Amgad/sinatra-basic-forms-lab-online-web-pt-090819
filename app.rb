479842ba62398d19a7e14a0d09ff320c18d42c84
Amgad Malik <amgadmalik@yahoo.com>
2019-12-29 19:27:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
   
   post '/puppy' do
-    @puppy_name = Puppy.new(params[:user_text])
+    @puppy_name = Puppy.new(params[:name], params[:breed], params[:age])
 
     erb :display_puppy
   end
